a5d080f84c103ac1884370ee4ceab79d9ff33fb2
Jake Loresch <loresch@gmail.com>
2019-03-11 20:38:56 -0500

    Initial Commit

---
+++ NewTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewTask
+//
+//  Created by Jake Loresch on 3/11/19.
+//  Copyright © 2019 Jake Loresch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NewTaskTests/NewTaskTests.swift
@@ -0,0 +1,34 @@
+//
+//  NewTaskTests.swift
+//  NewTaskTests
+//
+//  Created by Jake Loresch on 3/11/19.
+//  Copyright © 2019 Jake Loresch. All rights reserved.
+//
+
+import XCTest
+@testable import NewTask
+
+class NewTaskTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
